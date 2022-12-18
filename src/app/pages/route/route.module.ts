3df1ad6115d1103c36bbea6355b6e4dbf0c8631d
JaceMaster <joseacajina85@gmail.com>
2022-12-18 16:48:27 -0600

    21

--- src/app/pages/route/route.module.ts
+++ src/app/pages/route/route.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouteComponent } from './route.component';
 import { RouteRoutingModule } from './route-routing.module';
+import { AddRouteComponent } from './add-route/add-route.component';
 
 
 
 @NgModule({
   declarations: [
-    RouteComponent
+    RouteComponent,
+    AddRouteComponent
   ],
   imports: [
     CommonModule,
