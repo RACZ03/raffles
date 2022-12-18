6ad292bc0760187878f76db4ac6d5a0a6d7fe6b0
JaceMaster <joseacajina85@gmail.com>
2022-12-18 16:48:44 -0600

    21

---
+++ src/app/pages/route/add-route/add-route.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AddRouteComponent } from './add-route.component';
+
+describe('AddRouteComponent', () => {
+  let component: AddRouteComponent;
+  let fixture: ComponentFixture<AddRouteComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AddRouteComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(AddRouteComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/route/add-route/add-route.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-route',
+  templateUrl: './add-route.component.html',
+  styleUrls: ['./add-route.component.scss']
+})
+export class AddRouteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
