7c65087fbeaadbce9ea2edc783debef16b216b19
Jose Cajina <joseacajina85@gmail.com>
2023-03-16 22:59:10 -0600

    Numeros Ganadores Finalizado

--- src/app/pages/ganador/addwinner/addwinner.component.ts
+++ src/app/pages/ganador/addwinner/addwinner.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, PatternValidator, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { NgbCalendar, NgbDate, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { WinnerService } from 'src/app/@core/services/winner.service';
 import { AlertService } from 'src/app/@core/utils/alert.service';
@@ -12,9 +12,26 @@ import * as moment from 'moment';
 })
 export class AddwinnerComponent implements OnInit {
 
-  fechaSorteo = new FormControl('',[Validators.required]);
-  numeroGanador = new FormControl('',[Validators.required,Validators.pattern('^[0-9]+')]);
+  fechaSorteo = new FormControl('',[Validators.required, this.fechaValida]);
+  numeroGanador = new FormControl('',[Validators.required,Validators.pattern('^[0-9]+'),this.numeroNegativo]);
   selected = new FormControl('',[Validators.required]);
+
+  fechaValida(control: FormControl): { [s: string]: boolean } {
+    const fechaIngresada = new Date(control.value);
+    const fechaActual = new Date();
+    if (fechaIngresada > fechaActual) {
+      return { fechaInvalida: true };
+    }
+    return {};
+  }
+
+  numeroNegativo(control: FormControl): { [s: string]: boolean } {
+    const numeroIngresado = control.value;
+    if (numeroIngresado < 0) {
+      return { numeroNegativo: true };
+    }
+    return {};
+  }
 
   ngbDatepicker: NgbDate | undefined;
   public model : NgbDate | undefined;
@@ -59,18 +76,52 @@ export class AddwinnerComponent implements OnInit {
 
 async  onSubmit() {
 
-    let resp = await this.winnerSvc.verifywinner(moment(this.fechaSorteo.value).format('YYYY-MM-DD'), this.selected.value );
+    let resp1 = await this.winnerSvc.verifywinner(moment(this.fechaSorteo.value).format('YYYY-MM-DD'), this.selected.value );
+    let { status,comment } = resp1;
+    if(status==200){
+      if (this.winnwerForm.valid) {
+        let data = {
+          fecha: moment(this.fechaSorteo.value).format('YYYY-MM-DD'),
+          numeroGanador: this.numeroGanador.value,
+          idSorteo: this.selected.value
+        }
+        let resp =  await this.winnerSvc.addWinner(data);
+        let { status,comment,message } = resp;
+        if(status==200){
+          this.alertSvc.showAlert(1,message,comment);
+          this.close();
+        }else{
+          this.alertSvc.showAlert(4,message,comment);
+          this.close();
+        }
+      }
+    }else
+    {
+      let confirm = await this.alertSvc.showConfirmLimit(comment,'Desea Guardar el numero ganador:'+this.numeroGanador.value+'?','Guardar');
+      if(confirm){
+        let data = {
+          fecha: moment(this.fechaSorteo.value).format('YYYY-MM-DD'),
+          numeroGanador: this.numeroGanador.value,
+          idSorteo: this.selected.value
+        }
+        let resp =  await this.winnerSvc.addWinner(data);
+        let { status,comment,message } = resp;
+        if(status==200){
+          this.alertSvc.showAlert(1,message,comment);
+          this.close();
+        }else{
+          this.alertSvc.showAlert(4,message,comment);
+          this.close();
+        }
+      }else{
+        this.alertSvc.showAlert(3,'Cancelado','Usuario cancelo la operacion');
+        this.close();
+      }
+
+
+    }
     
-       if (this.winnwerForm.valid) {
-         let data = {
-           fecha: moment(this.fechaSorteo.value).format('YYYY-MM-DD'),
-           numeroGanador: this.numeroGanador.value,
-           idSorteo: this.selected.value
-         }
-         console.log(data);
-         let resp =  await this.winnerSvc.addWinner(data);
-        
-       }
+   
   }
 
   close() {
--- src/app/pages/ganador/list-ganador/list-ganador.component.ts
+++ src/app/pages/ganador/list-ganador/list-ganador.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { DataTableDirective } from 'angular-datatables';
+import * as moment from 'moment';
 import { Subject } from 'rxjs';
 import { WinnerService } from 'src/app/@core/services/winner.service';
+import { AlertService } from 'src/app/@core/utils/alert.service';
 import { DataTableServiceService } from 'src/app/@core/utils/data-table-service.service';
+import { ExporterDataService } from 'src/app/@core/utils/exporter-data.service';
 
 @Component({
   selector: 'app-list-ganador',
@@ -21,7 +24,9 @@ export class ListGanadorComponent implements OnInit {
 
   constructor(
     private dataTableSvc: DataTableServiceService,
-    private winnerSvc: WinnerService
+    private winnerSvc: WinnerService,
+    private alertSvc: AlertService,
+    private exportSvc: ExporterDataService
   ) { 
     this.dtOptions = this.dataTableSvc.dtOptions || {};
     this.loadData();
@@ -34,13 +39,35 @@ export class ListGanadorComponent implements OnInit {
     this.data = [];
     let resp = await this.winnerSvc.getwinners();
      let { data,status,message,comment } = resp;
-     console.log(data);
      if(status==200){
         this.data = data;
+      }else{
+        this.alertSvc.showAlert(4,comment,message);
       }
 
 
     this.dtTrigger.next(this.dtOptions);
+  }
+
+  //elimnar ganador
+  async deleteWinner(id: number) {
+    let confirm = await this.alertSvc.showConfirm(
+      'Eliminar ganador',
+      '¿Está seguro de eliminar el ganador?'
+    );
+    if (!confirm) return this.alertSvc.showAlert(2, 'Cancelado', 'No se eliminó el ganador');
+
+    let resp = await this.winnerSvc.deleteWinner(id);
+      let { status, message, comment } = resp;
+      if (status == 200) {
+        this.alertSvc.showAlert(1, message, comment);
+        this.dataTableSvc.dtElements = this.dtElement;
+        this.renderer();
+      } else {
+        this.alertSvc.showAlert(4, comment, message);
+      }
+      
+    
   }
 
   addWinner() {
@@ -71,5 +98,43 @@ export class ListGanadorComponent implements OnInit {
     // this.renderer
     this.dtTrigger.unsubscribe();
   }
+
+    /* Search */
+    searchData(e: any) {
+      let value = e.target.value;
+      this.dtElement.dtInstance.then((dtInstance: DataTables.Api) => {
+        dtInstance.search(value).draw();
+      });
+    }
+
+    exportToExcel() {
+      let json = this.data.map((item: any) => {
+        return {
+          'Fecha': moment(item?.fecha).format('DD/MM/YYYY'),
+          'Sorteo':item?.sorteo?.nombre,
+          'Ventas Totales':item?.ventasTotales,
+          'Numero Ganador':item?.numeroGanador,
+          'Inversion al Ganador':item?.inversionAlGanador,
+          'Premio Total':item?.premioTotal, 
+          'Utilidad':item?.utilidad 
+        }
+      });
+      this.exportSvc.exportToExcel(json, 'negocios');
+    }
+  
+    exportToPDF() {
+      let json = this.data.map((item: any) => {
+        return {
+          'Fecha': moment(item?.fecha).format('DD/MM/YYYY'),
+          'Sorteo':item?.sorteo?.nombre,
+          'Ventas Totales':item?.ventasTotales,
+          'Numero Ganador':item?.numeroGanador,
+          'Inversion al Ganador':item?.inversionAlGanador,
+          'Premio Total':item?.premioTotal, 
+          'Utilidad':item?.utilidad 
+        }
+      });
+      this.exportSvc.exportPdf(json, 'negocios');
+    }
 
 }
