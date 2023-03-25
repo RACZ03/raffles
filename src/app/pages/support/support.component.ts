36716144a3ef41005c211982cd76152114b3797c
Obed Navarrete <ndiazobed@gmail.com>
2023-03-25 13:58:22 -0600

    m

--- src/app/pages/support/support.component.ts
+++ src/app/pages/support/support.component.ts
@@ -20,6 +20,28 @@ declare const navigator: any;
 })
 export class SupportComponent implements OnInit {
 
+  public documentDefinition: TDocumentDefinitions = {
+    pageSize: 'A6',
+    pageOrientation: 'portrait',
+    content: [
+      {
+        text: `RECIBO ${ 12312312 }`,
+        bold: true,
+        fontSize: 12,
+        alignment: 'center',
+        margin: [0, 1],
+      },
+      {
+        text: `FECHA ${ new Date().toLocaleDateString() }`,
+        bold: true,
+        fontSize: 12,
+        alignment: 'center',
+        margin: [0, 1],
+      },
+    ],
+  };
+  public pdf: any = pdfMake.createPdf(this.documentDefinition);
+
   constructor(
     private alertSvc: AlertService,
   ) { }
@@ -32,81 +54,90 @@ export class SupportComponent implements OnInit {
 
   findPrinter(band = false) {
     // verify if navigator.bluetooth exists
-    if ( !navigator.bluetooth) {
-      // this.alertSvc.showAlert(4, 'Error', 'Web Bluetooth API is not available in this browser.');
+    // if ( !navigator.bluetooth) {
+    //   // this.alertSvc.showAlert(4, 'Error', 'Web Bluetooth API is not available in this browser.');
 
 
-      // create pdf and send to print pdf
-      const documentDefinition: TDocumentDefinitions = {
-        pageSize: 'A6',
-        pageOrientation: 'portrait',
-        content: [
-          {
-            text: `RECIBO ${ 12312312 }`,
-            bold: true,
-            fontSize: 12,
-            alignment: 'center',
-            margin: [0, 1],
-          },
-          {
-            text: `FECHA ${ new Date().toLocaleDateString() }`,
-            bold: true,
-            fontSize: 12,
-            alignment: 'center',
-            margin: [0, 1],
-          },
-        ],
-      };
-      let pdf = pdfMake.createPdf(documentDefinition);
+    //   // create pdf and send to print pdf
+    //   const documentDefinition: TDocumentDefinitions = {
+    //     pageSize: 'A6',
+    //     pageOrientation: 'portrait',
+    //     content: [
+    //       {
+    //         text: `RECIBO ${ 12312312 }`,
+    //         bold: true,
+    //         fontSize: 12,
+    //         alignment: 'center',
+    //         margin: [0, 1],
+    //       },
+    //       {
+    //         text: `FECHA ${ new Date().toLocaleDateString() }`,
+    //         bold: true,
+    //         fontSize: 12,
+    //         alignment: 'center',
+    //         margin: [0, 1],
+    //       },
+    //     ],
+    //   };
+    //   let pdf = pdfMake.createPdf(documentDefinition);
 
-      // open modal to print pdf
-      pdf.getBase64((data) => {
-        printJS({
-          printable: data,
-          type: 'pdf',
-          base64: true,
-          showModal: true,
-          modalMessage: 'Imprimiendo...',
+    //   // open modal to print pdf
+    //   pdf.getBase64((data) => {
+    //     printJS({
+    //       printable: data,
+    //       type: 'pdf',
+    //       base64: true,
+    //       showModal: true,
+    //       modalMessage: 'Imprimiendo...',
 
-        });
+    //     });
+    //   });
+
+    //   return;
+
+    // }
+
+    navigator.bluetooth.requestDevice({
+      filters: [{
+        // services: ['public-a001']
+        services: ['000018f0-0000-1000-8000-00805f9b34fb']
+      }]
+    })
+    .then((device: any) => {
+      console.log('Impresora encontrada:', device);
+      this.alertSvc.showAlert(1, 'Impresora encontrada', device.name);
+      this.device = device;
+      // set device in localstorage
+      localStorage.setItem('devicePrint', JSON.stringify(device));
+      return device.gatt.connect();
+    })
+    .then((server: any) => {
+      this.alertSvc.showAlert(1, 'Conectado', 'Conectado a la impresora');
+      // return server.getPrimaryService('public-a001');
+      return server.getPrimaryService('000018f0-0000-1000-8000-00805f9b34fb');
+    })
+    .then((service: any) => {
+      // Aquí puedes utilizar los métodos y características del servicio para enviar datos de impresión a la impresora.
+      // Por ejemplo:
+      // service.getCharacteristic('public-a002').then((characteristic) => {
+      //   let data = new Uint8Array([0x41, 0x42, 0x43]); // Datos de impresión
+      //   characteristic.writeValue(data);
+      // });
+      //  create document pdf
+      this.pdf.getBase64((data: any) => {
+          printJS({
+            printable: data,
+            type: 'pdf',
+            base64: true,
+            showModal: true,
+            modalMessage: 'Imprimiendo...',
+          });
       });
-
-      return;
-
-    }
-
-    // navigator.bluetooth.requestDevice({
-    //   filters: [{
-    //     // services: ['public-a001']
-    //     services: ['000018f0-0000-1000-8000-00805f9b34fb']
-    //   }]
-    // })
-    // .then((device: any) => {
-    //   console.log('Impresora encontrada:', device);
-    //   this.alertSvc.showAlert(1, 'Impresora encontrada', device.name);
-    //   this.device = device;
-    //   // set device in localstorage
-    //   localStorage.setItem('devicePrint', JSON.stringify(device));
-    //   return device.gatt.connect();
-    // })
-    // .then((server) => {
-    //   this.alertSvc.showAlert(1, 'Conectado', 'Conectado a la impresora');
-    //   // return server.getPrimaryService('public-a001');
-    //   return server.getPrimaryService('000018f0-0000-1000-8000-00805f9b34fb');
-    // })
-    // .then((service) => {
-    //   // Aquí puedes utilizar los métodos y características del servicio para enviar datos de impresión a la impresora.
-    //   // Por ejemplo:
-    //   // service.getCharacteristic('public-a002').then((characteristic) => {
-    //   //   let data = new Uint8Array([0x41, 0x42, 0x43]); // Datos de impresión
-    //   //   characteristic.writeValue(data);
-    //   // });
-    //   this.print();
-    // })
-    // .catch((error) => {
-    //   console.error(error);
-    //   this.alertSvc.showAlert(4, 'Error', error);
-    // });
+    })
+    .catch((error: any) => {
+      console.error(error);
+      this.alertSvc.showAlert(4, 'Error', error);
+    });
   }
 
   async print() {
