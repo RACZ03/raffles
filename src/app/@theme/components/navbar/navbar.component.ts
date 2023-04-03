9c6e2b12d0b58e69bbe2e5c212d4c41d5d096d93
Roberto Calero <roberto.calero@claro.com.ni>
2023-04-02 18:12:41 -0600

    Configuracion de rutas y ajustes en tickets

--- src/app/@theme/components/navbar/navbar.component.ts
+++ src/app/@theme/components/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 
 import { Component, OnInit, ElementRef } from '@angular/core';
-import { ROUTES } from '../sidebar/sidebar.component';
 import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/@core/services/auth.service';
+import { UsersService } from 'src/app/@core/services/users.service';
+import { ROUTES_ADMIN, ROUTES_SALES, ROUTES_SUPERVISOR, ROUTES_SUPER_ADMIN } from '../sidebar/sidebar.component';
 
 @Component({
   selector: 'app-navbar',
@@ -20,14 +21,29 @@ export class NavbarComponent implements OnInit {
     location: Location,
     private element: ElementRef,
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private userSvc: UsersService
   ) {
     this.location = location;
     this.identity = JSON.parse(localStorage.getItem('identity') || '{}');
+
+    let isSuperAdmin = this.userSvc.verifyRole('ROLE_SUPER_ADMIN');
+    let isAdmin = this.userSvc.verifyRole('ROLE_ADMIN');
+    let isSupervisor = this.userSvc.verifyRole('ROLE_SUPERVISOR');
+
+    if ( isSuperAdmin ) {
+      this.listTitles = ROUTES_SUPER_ADMIN.filter(listTitle => listTitle);
+    } else if ( isAdmin ) {
+      this.listTitles = ROUTES_ADMIN.filter(listTitle => listTitle);
+    } else if ( isSupervisor ) {
+      this.listTitles = ROUTES_SUPERVISOR.filter(listTitle => listTitle);
+    } else {
+      this.listTitles = ROUTES_SALES.filter(listTitle => listTitle);
+    }
   }
 
   ngOnInit() {
-    this.listTitles = ROUTES.filter(listTitle => listTitle);
+    // this.listTitles = ROUTES.filter(listTitle => listTitle);
   }
   getTitle(){
     var path = this.location.prepareExternalUrl(this.location.path());
