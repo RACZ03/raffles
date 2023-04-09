cb628dd969c80bfab9fff01dfefb1ce2cd8784fc
JaceMaster <joseacajina85@gmail.com>
2023-04-09 11:35:32 -0600

    Cambios en reporte

--- src/app/pages/report/report/report.component.ts
+++ src/app/pages/report/report/report.component.ts
@@ -9,11 +9,14 @@ import { Router } from '@angular/router';
 export class ReportComponent implements OnInit {
 
   public showMenu: boolean = true;
+  public mostrar = false;
+  public mostrarAdminSuper = false;
   constructor(
     private route: Router
   ) { }
 
   ngOnInit(): void {
+    this.validaRole();
     // validate if url contains /pages/report/ and hide menu
     if (this.route.url.includes('/pages/report/')) {
       this.showMenu = false;
@@ -31,7 +34,19 @@ export class ReportComponent implements OnInit {
     });
   }
 
+  validaRole() {
+    let Roles = JSON.parse(localStorage.getItem('roles') || '{}');
+    for (const item of Roles) {
+      if(item.nombre == 'ROLE_SUPER_ADMIN'){
+        this.mostrar = true;
+        this.mostrarAdminSuper = true;
+      }
+      if(item.nombre == 'ROLE_ADMIN'){
+        this.mostrarAdminSuper = true;
+      }
 
+    }
+  }
 
 
 }
