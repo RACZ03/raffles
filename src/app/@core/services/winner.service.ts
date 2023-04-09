95ba5144f21911edd08077df62db9be55d487113
JaceMaster <joseacajina85@gmail.com>
2023-04-08 18:32:17 -0600

    Modulo de Ganadores

--- src/app/@core/services/winner.service.ts
+++ src/app/@core/services/winner.service.ts
@@ -25,23 +25,26 @@ export class WinnerService {
   getwinners(): Promise<any> {
     //ganador/porNegocio/4
     let identity = this.getIdentity();
+    let negocio = JSON.parse(localStorage.getItem('business') || '{}');
     //varaible booleana
-    let isSuperAdmin = false;
+    let isAdmin = false;
 
   for (const item of identity.roles) {
-   if(item.nombre == 'ROLE_SUPER_ADMIN'){
-    isSuperAdmin = true;
+   if(item.nombre == 'ROLE_ADMIN'){
+    isAdmin = true;
    }
   }
-   if(!isSuperAdmin){
-    return this.connectionSvc.send('get', `ganador/porNegocio/${identity.idNegocio}`);
+   if(isAdmin){
+    return this.connectionSvc.send('get', `ganador/porNegocio/${negocio.idNegocio}`);
    }
-   else{
+
     return this.connectionSvc.send('get', `ganador/todos`);
-   }
 
 }
 
+getwinnersByBusiness(idNegocio: any): Promise<any> {
+  return this.connectionSvc.send('get', `ganador/porNegocio/${idNegocio}`);
+}
 
   getSorteo(): Promise<any> {
     return this.connectionSvc.send('get', `sorteo/obtener`);
@@ -58,6 +61,18 @@ export class WinnerService {
   deleteWinner(id: number): Promise<any> {
     return this.connectionSvc.send('delete', `ganador/eliminar/${id}`);
   }
+
+  //ganador/vendedoresPorGanadorNegocio/7/1
+  getVendedoresPorGanadorNegocio(idGanador: any, idNegocio: any): Promise<any> {
+    return this.connectionSvc.send('get', `ganador/vendedoresPorGanadorNegocio/${idGanador}/${idNegocio}`);
+  }
+
+  //ganador/rutasPorGanadorNegocio/7/4
+  getRutasPorGanadorNegocio(idGanador: any, idNegocio: any): Promise<any> {
+    return this.connectionSvc.send('get', `ganador/rutasPorGanadorNegocio/${idGanador}/${idNegocio}`);
+  }
+
+
 
 
 }
